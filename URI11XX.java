3d6412813ddec658ec6cc13a4ddc5f5a375d17f8
alexchti <alexchx@gmail.com>
2020-09-16 14:59:17 -0300

    versao final sala de aula

--- URI11XX.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class URI1144{
-    public static void main(String args[]){
-        Scanner teclado = new Scanner(System.in);
-
-        xxx = teclado.nextInt();
-    }
-}
--- URIXXX copy 2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class URIXXXX{
-    public static void main(String args[]){
-        Scanner teclado = new Scanner(System.in);
-
-        xxx = teclado.nextInt();
-    }
-}
